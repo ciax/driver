ccdc6a33d24ee4b71647741d4be4048f0159d295
CIAX <ciax@clx-i.sum.naoj.org>
2009-01-09 12:08:19 -1000

    modify logtimeshift

--- bin/logtimeshift.sh
+++ bin/logtimeshift.sh
@@ -3,17 +3,18 @@
 ts="timeshift.txt"
 [ -f $ts ] || (ssh ciax date;date) > $ts
 diff=$(set - $(date -f $ts +%s);echo $(( $2 - $1 )))
+echo "DIFF = $diff sec"
+temp=".tmp$(date +%s).log"
 for file ; do
-    echo "DIFF = $diff sec"
     IFS=$'\n'
     for l in $(< $file); do
 	old="${l:0:6} ${l:7:2}:${l:9:2}:${l:11:2}"
 	new=$(date -d "$old $diff second ago" +%y%m%d-%H%M%S)
-	if [ ! "$newfile" ] ; then
-	    newfile="${file%-*}-${new%-*}.log"
-	    [ -f $newfile ] && rm $newfile
-	fi
-	echo "$new${l:13}" >> $newfile
+	[ "$begin" ] || begin=${new%-*}
+	echo "$new${l:13}" >> $temp
     done
+    end=${new%-*}
+    newfile="${file%%-*}-$begin-$end.log"
+    mv $temp $newfile
     echo "$newfile is created!"
 done
