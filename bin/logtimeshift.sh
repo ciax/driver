70709abba81947ea9eaef205b503b6dfac2ab17a
CIAX <ciax@ciax.sum.naoj.org>
2009-01-09 11:45:11 -1000

    modify logtimeshift -> create file

--- bin/logtimeshift.sh
+++ bin/logtimeshift.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-[ -f "$1" ] || { echo "Usage: ${0##*/} [logfile]"; exit 1; }
-file="$1"
+[ -f "$1" ] || { echo "Usage: ${0##*/} [logfiles]"; exit 1; }
 ts="timeshift.txt"
-[ -f $ts ] || { echo "NEED $ts"; exit 1; }
-set - `date -f $ts +%s`
-diff=$(( $2 - $1 ))
-echo "DIFF=$diff"
-
-IFS=$'\n'
-
-for line in `cat $file`; do
-    IFS=$' '
-    set - $line
-    time="${1:0:6} ${1:7:2}:${1:9:2}:${1:11:2}"
-    shift
-    body="$*"
-    new=`date -d "$time $diff second ago" +%y%m%d-%H%M%S`
-    echo "$new $body"
+[ -f $ts ] || (ssh ciax date;date) > $ts
+diff=$(set - $(date -f $ts +%s);echo $(( $2 - $1 )))
+for file ; do
+    echo "DIFF = $diff sec"
+    IFS=$'\n'
+    for l in $(< $file); do
+	old="${l:0:6} ${l:7:2}:${l:9:2}:${l:11:2}"
+	new=$(date -d "$old $diff second ago" +%y%m%d-%H%M%S)
+	if [ ! "$newfile" ] ; then
+	    newfile="${file%-*}-${new%-*}.log"
+	    [ -f $newfile ] && rm $newfile
+	fi
+	echo "$new${l:13}" >> $newfile
+    done
+    echo "$newfile is created!"
 done
